b9792714c3effcc06704f88a2a3374becfa0ed7b
Ryuubii <prexordrake@gmail.com>
2021-11-28 15:38:03 +0700

    + Admin User List Prep

--- app/src/main/java/com/example/foodiepedia/Fragments/AdminUserListFragment.java
+++ app/src/main/java/com/example/foodiepedia/Fragments/AdminUserListFragment.java
@@ -2,7 +2,10 @@ package com.example.foodiepedia.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,33 +20,15 @@ import com.example.foodiepedia.R;
  */
 public class AdminUserListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    RecyclerView rvUsers;
 
     public AdminUserListFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminUserListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdminUserListFragment newInstance(String param1, String param2) {
+    public static AdminUserListFragment newInstance() {
         AdminUserListFragment fragment = new AdminUserListFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -51,10 +36,6 @@ public class AdminUserListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -62,5 +43,12 @@ public class AdminUserListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_admin_user_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvUsers = view.findViewById(R.id.rvUsers);
+
     }
 }
